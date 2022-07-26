adf0d05e9f24c347111ff85fbee33c0c2fa0c24d
choidoorim <doorim321@naver.com>
2022-07-26 22:35:54 +0900

    style: airbnb style guide 적용

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,22 +3,34 @@ module.exports = {
   parserOptions: {
     project: 'tsconfig.json',
     sourceType: 'module',
+    ecmaFeatures: {
+      jsx: false,
+    },
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
   root: true,
   env: {
+    browser: false,
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'node_modules/', 'dist/'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'require-await': 'error',
+    'class-methods-use-this': 'off',
+    'import/prefer-default-export': 'off',
+    'no-underscore-dangle': 'warn',
+    'no-return-await': 'error',
+    'max-classes-per-file': 'off',
   },
 };
