1655cb0a2e479bf51c89f44e9ce83419d69c45b9
choidoorim <doorim321@naver.com>
2022-08-01 21:27:45 +0900

    feat: env 설정

---
+++ environments/env-config.schema.ts
@@ -0,0 +1,5 @@
+import * as Joi from 'joi';
+
+export default Joi.object({
+  DATABASE_URL: Joi.string().required(),
+});
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import validationSchema from '@env/env-config.schema';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`environments/.${process.env.NODE_ENV}.env`],
+      isGlobal: true,
+      validationSchema,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
