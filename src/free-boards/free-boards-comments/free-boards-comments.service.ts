4e63a9b8ae379e8414dcb130943bdf931c2e735f
Ahn <dkswnsdud159@naver.com>
2022-08-25 01:32:00 +0900

    댓글 생성 유효성 검증

--- src/free-boards/free-boards-comments/free-boards-comments.service.ts
+++ src/free-boards/free-boards-comments/free-boards-comments.service.ts
@@ -1,14 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { isEmpty } from 'class-validator';
 import { CreateFreeBoardsCommentsDto } from './dto/create-free-boards-comments.dto';
 import { FreeBoardsCommentsRepository } from './free-boards-comments.repository';
+import { FreeBoardsService } from '../free-boards.service';
 
 @Injectable()
 export class FreeBoardsCommentsService {
   constructor(
     private readonly freeBoardsCommentsRepository: FreeBoardsCommentsRepository,
+    private readonly freeBoardsService: FreeBoardsService,
   ) {}
 
   create(createFreeBoardsCommentsDto: CreateFreeBoardsCommentsDto) {
+    const isFreeBoardId = this.freeBoardsService.findById(
+      createFreeBoardsCommentsDto.freeBoardId,
+    );
+
+    if (isEmpty(isFreeBoardId)) {
+      throw new ForbiddenException('존재하지 않는 게시글 입니다.');
+    }
+
     return this.freeBoardsCommentsRepository.create(
       createFreeBoardsCommentsDto,
     );
