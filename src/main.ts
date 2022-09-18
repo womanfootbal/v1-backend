862c6ed71ef9468d72906005626454781c0a6d2b
ChoiDoorim <doorim321@naver.com>
2022-09-19 00:05:46 +0900

    feat: cors 설정 추가 (#34)

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:3001'],
+    credentials: true,
+    methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  });
   app.enableVersioning();
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
